83c9d5bbd537ec0f39a48b5dd5a0f0dd78ffadcc
BCCIA <tecnologia@bccvzla.com>
2025-02-11 14:11:26 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,125 @@
+// Deshabilitar clic derecho en la página principal
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// Función para deshabilitar combinaciones de teclas (F12, Ctrl+Shift+I, etc.)
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, "I") ||
+    ctrlShiftKey(e, "J") ||
+    ctrlShiftKey(e, "C") ||
+    (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+  )
+    return false;
+};
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+class DIDChat {
+  constructor(containerId) {
+    this.container = document.getElementById(containerId);
+    // AQUI VAS A PODER MODIFICAR EL LINK CUANDO QUIERES ASEGURATE DE PONER EL LINK EN COMILLAS. EJEMPLO:"https://studio.d-id.com";
+    this.chatUrl =
+      "https://studio.d-id.com/agents/share?id=agt_1pM2Ko9X&utm_source=copy&key=WjI5dloyeGxMVzloZFhSb01ud3hNVEU0T0RjNU1UQTRNamMxTlRBNU9EYzJNakU2VjNWc1prRTVjMEpwUkhSaGVubERWSGN0ZERGaA==";
+    this.iframe = null;
+    this.init();
+  }
+
+  init() {
+    this.createIframe();
+  }
+
+  createIframe() {
+    const wrapper = document.createElement("div");
+    wrapper.className = "iframe-wrapper";
+    this.iframe = document.createElement("iframe");
+    this.iframe.className = "did-chat-iframe fade-in";
+    this.iframe.src = this.chatUrl;
+    this.iframe.allow = "camera;microphone;clipboard-write";
+    this.iframe.title = "D-ID Chat Interface";
+
+    wrapper.appendChild(this.iframe);
+    this.container.appendChild(wrapper);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const chat = new DIDChat("chat-container");
+});
+
+gsap.to(".first", 1.5, {
+  delay: 0.5,
+  top: "-100%",
+  ease: Expo.easeInOut,
+});
+
+gsap.to(".second", 1.5, {
+  delay: 0.7,
+  top: "-100%",
+  ease: Expo.easeInOut,
+});
+
+gsap.to(".third", 1.5, {
+  delay: 0.9,
+  top: "-100%",
+  ease: Expo.easeInOut,
+});
+
+gsap.from(".home-img", { opacity: 0, duration: 2, delay: 2, x: 60 });
+
+gsap.from(".home-information", {
+  opacity: 0,
+  duration: 3,
+  delay: 2.3,
+  y: 25,
+});
+
+gsap.from(".anime-text", {
+  opacity: 0,
+  duration: 3,
+  delay: 2.3,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.3,
+});
+
+gsap.from(".nav-logo", {
+  opacity: 0,
+  duration: 3,
+  delay: 3.2,
+  y: 25,
+  ease: "expo.out",
+});
+
+gsap.from(".nav-item", {
+  opacity: 0,
+  duration: 3,
+  delay: 3.2,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".home-social", {
+  opacity: 0,
+  duration: 3,
+  delay: 4,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
