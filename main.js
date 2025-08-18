8bb4dbe31ddd6f23575c7a5073a675337f99aa25
BCCIA <tecnologia@bccvzla.com>
2025-08-18 10:02:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ class DIDChat {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
     this.chatUrl =
-      "https://studio.d-id.com/agents/share?id=agt_IFidIPzC&utm_source=copy&key=WjI5dloyeGxMVzloZFhSb01ud3hNVEU0T0RjNU1UQTRNamMxTlRBNU9EYzJNakU2VjNWc1prRTVjMEpwUkhSaGVubERWSGN0ZERGaA==";
+      "https://studio.d-id.com/agents/share?id=v2_agt_YpRGSGR5&utm_source=copy&key=WjI5dloyeGxMVzloZFhSb01ud3hNRGcyTmprME9EYzBNREExTmpNeU5UYzBNREk2UlVZNE1YRTBRMHhXTW1oUmRtUlFjRlkwZVVWbA==";
     this.iframe = null;
     this.init();
   }
